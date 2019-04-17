03d59f3ee85c1bf8baf6d828aa4f549edb78594f
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-16 21:07:34 -0400

    Adding functionality to remove an image

--- image.js
+++ image.js
@@ -6,3 +6,14 @@ function deleteImage() {
     // removing errors on button clicks
     removeError();
     var inputurl = document.getElementById("input_url").value;
+    if (isValidUrl(inputurl)) {
+
+        // remove only one image
+        var index = 0;
+        for (index = 0; index < imagePlanning.length; index += 1) {
+            if (imagePlanning[index] === inputurl) {
+                imagePlanning.splice(index, 1);
+                displayPlanningArea();
+                return;
+            }
+        }
