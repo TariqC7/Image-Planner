945cbc6f77af1819c3c7738edb69e03c8f27aefb
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-16 21:03:44 -0400

    Function to delete images
    Here I'm removing errors on button clicks

--- image.js
+++ image.js
@@ -1,1 +1,8 @@
+"use strict";
 
+var imagePlanning = [];
+
+function deleteImage() {
+    // removing errors on button clicks
+    removeError();
+    var inputurl = document.getElementById("input_url").value;
